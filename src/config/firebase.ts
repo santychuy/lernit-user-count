bab5ec313545980b514eccaf6ca94058a497c186
Santiago Carrasco <jsanticc@hotmail.com>
2020-06-03 20:21:30 -0700

    Las details

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,15 +1,10 @@
-import firebase from 'firebase';
-
-(() => {
-  // TODO: Guardar en Variables de Entorno
-  firebase.initializeApp({
-    apiKey: 'AIzaSyCj50GE0ICHYfiJ6qSG51WqhYHJFs7FPow',
-    authDomain: 'lernit-user-count.firebaseapp.com',
-    databaseURL: 'https://lernit-user-count.firebaseio.com',
-    projectId: 'lernit-user-count',
-    storageBucket: 'lernit-user-count.appspot.com',
-    messagingSenderId: '846889448479',
-    appId: '1:846889448479:web:7dc3a5748945b1d7bcd12a',
-    measurementId: 'G-1TZ2ND2JYN',
-  });
-})();
+export default {
+  apiKey: 'AIzaSyCj50GE0ICHYfiJ6qSG51WqhYHJFs7FPow',
+  authDomain: 'lernit-user-count.firebaseapp.com',
+  databaseURL: 'https://lernit-user-count.firebaseio.com',
+  projectId: 'lernit-user-count',
+  storageBucket: 'lernit-user-count.appspot.com',
+  messagingSenderId: '846889448479',
+  appId: '1:846889448479:web:7dc3a5748945b1d7bcd12a',
+  measurementId: 'G-1TZ2ND2JYN',
+};
--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,9 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { FirebaseAppProvider } from 'reactfire';
 
-// import './config/firebase';
+import firebaseConfig from './config/firebase';
 import App from './App';
 import { GlobalStyle } from './globalStyles';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyCj50GE0ICHYfiJ6qSG51WqhYHJFs7FPow',
-  authDomain: 'lernit-user-count.firebaseapp.com',
-  databaseURL: 'https://lernit-user-count.firebaseio.com',
-  projectId: 'lernit-user-count',
-  storageBucket: 'lernit-user-count.appspot.com',
-  messagingSenderId: '846889448479',
-  appId: '1:846889448479:web:7dc3a5748945b1d7bcd12a',
-  measurementId: 'G-1TZ2ND2JYN',
-};
 
 ReactDOM.render(
   <React.StrictMode>
